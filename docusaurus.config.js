f1c6589846a03345d5df555b375356a48721185f
Muh Veven <mhmmdveven@gmail.com>
2025-05-20 16:03:09 +0700

    Update docusaurus.config.js
    edit config
    
    title and tagline

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -10,8 +10,8 @@ import {themes as prismThemes} from 'prism-react-renderer';
 
 /** @type {import('@docusaurus/types').Config} */
 const config = {
-  title: 'My Site',
-  tagline: 'Dinosaurs are cool',
+  title: 'Veven's Blog',
+  tagline: 'Welcom to my Blog',
   favicon: 'img/favicon.ico',
 
   // Set the production url of your site here
@@ -76,7 +76,7 @@ const config = {
       // Replace with your project's social card
       image: 'img/docusaurus-social-card.jpg',
       navbar: {
-        title: 'My Site',
+        title: 'mhvvn',
         logo: {
           alt: 'My Site Logo',
           src: 'img/logo.svg',
